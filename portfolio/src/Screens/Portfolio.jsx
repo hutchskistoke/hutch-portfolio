87a66558852c1cf67c7cd2140868a80f6ad72019
Ashley Hutchinson <hutchski@Ashleys-MBP.fios-router.home>
2021-04-13 12:15:36 -0400

    proj page init

--- portfolio/src/Screens/Portfolio.jsx
+++ portfolio/src/Screens/Portfolio.jsx
@@ -4,7 +4,30 @@ import '../CSS/Portfolio.css'
 export default function Portfolio() {
   return (
     <div className='portfolio-container'>
-      <div>UNDER CONSTRUCTION</div>
+      <div className='proj-container'>
+        <img src='https://i.imgur.com/cgZ15Kk.png' alt='pets-r-us' className='proj-img'></img>
+        <div className='desc'>Pets-R-Us is a full-stack application built by myself and 3 other team members. </div>
+        <div className='buttons-container'>
+          <button className='code'><a href='https://github.com/jessicamyee/pets-r-us' target='_blank' rel="noreferrer">see the code</a></button>
+          <button className='site'><a href='https://pets-r-us.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
+        </div>  
+      </div>
+      <div className='proj-container'>
+        <img src='https://i.imgur.com/K51bRMa.png' alt='striva' className='proj-img'></img>
+        <div className='desc'>Striva is a mobile first full-stack web application.  </div>
+        <div className='buttons-container'>
+          <button className='code'><a href='https://github.com/hutchskistoke/Striva' target='_blank' rel="noreferrer">see the code</a></button>
+          <button className='site'><a href='https://striva.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
+        </div>  
+      </div>
+      <div className='proj-container'>
+        <img src='https://i.imgur.com/86cSM8J.png' alt='avy-savvy' className='proj-img'></img>
+        <div className='desc'>AvySavvy </div>
+        <div className='buttons-container'>
+          <button className='code'><a href='https://github.com/hutchskistoke/avy-savy' target='_blank' rel="noreferrer">see the code</a></button>
+          <button className='site'><a href='https://avy-savvy-snow.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
+        </div>  
+      </div>
     </div>
   )
 }
