ca690630e36faad41e93c81c59f526abb5622c0a
Ashley Hutchinson <hutchski@Ashleys-MBP.fios-router.home>
2021-04-13 15:21:03 -0400

    about intro

--- portfolio/src/Screens/About.jsx
+++ portfolio/src/Screens/About.jsx
@@ -4,7 +4,16 @@ import '../CSS/About.css'
 export default function About() {
   return (
     <div className='about-container'>
-      <div>UNDER CONSTRUCTION</div>
+      {/* <div>UNDER CONSTRUCTION</div> */}
+      <div className='words'>
+        Hi, I'm Ashley Hutchinson! I am a software engineer by trade and outdoor enthusiast by passion. My undergraduate studies were in Mathemtics but the past decade was spent exploring what this incredible world has to offer. Next adventure...
+      </div>
+      <div className='pun'>
+        World Wide Exploration <span className='span'>becomes</span> World Wide Web!
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
--- portfolio/src/Screens/Contact.jsx
+++ portfolio/src/Screens/Contact.jsx
@@ -5,6 +5,7 @@ export default function Contact() {
   return (
     <div className='contact-container'>
       <div className='connect-title'>Let's Connect!</div>
+      <div className='my-email'>(hutchskistoke@gmail.com)</div>
       <form action="https://getform.io/f/19e63ecb-697d-4caf-9436-97aa2d37f7a6" method="POST" className='form'>
         <label className='name-tag'>
           Name: 
