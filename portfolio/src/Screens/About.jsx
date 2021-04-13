acfab13e8867adf3de0a99a35476cdfffe5f16e8
Ashley Hutchinson <hutchski@Ashleys-MBP.fios-router.home>
2021-04-13 16:32:23 -0400

    for submission

--- portfolio/src/Screens/About.jsx
+++ portfolio/src/Screens/About.jsx
@@ -5,14 +5,20 @@ export default function About() {
   return (
     <div className='about-container'>
       {/* <div>UNDER CONSTRUCTION</div> */}
-      <div className='words'>
-        Hi, I'm Ashley Hutchinson! I am a software engineer by trade and outdoor enthusiast by passion. My undergraduate studies were in Mathemtics but the past decade was spent exploring what this incredible world has to offer. Next adventure...
+      <div className='words-one'>
+        Hi, I'm Ashley Hutchinson! I am a software engineer by trade and outdoor enthusiast by passion. My undergraduate studies were in Mathematics but the past decade was spent exploring what this incredible world has to offer. Now for my next adventure...
       </div>
       <div className='pun'>
         World Wide Exploration <span className='span'>becomes</span> World Wide Web!
       </div>
-      <div>
-
+      <div className='words-two'>
+        My decade of fun was made possible through my work as a bartender. The food and beverage industry has shaped me to thrive in fast paced and highly demanding work environments where maintaining a positive experience for clientele is priority number one. 
+      </div>
+      <div className='words-three'>
+        My ten year experience at this Forbes rated resort led me to a supervisory role which gave me the opportinity to handle things such as team management, inventory, ordering, scheduling, etc. 
+      </div>
+      <div className='words-four'>
+        I am excited to bring both my professional and personal background skills with me in to this next journey in the tech world! 
       </div>
     </div>
   )
