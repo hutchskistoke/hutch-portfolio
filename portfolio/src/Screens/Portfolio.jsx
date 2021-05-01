8cf7239d52b953a6e07efd95f4ef349bf63a9490
Ashley Hutchinson <hutchski@Ashleys-MacBook-Pro.local>
2021-05-01 10:01:55 -0600

    nav edits

--- portfolio/src/Screens/Portfolio.jsx
+++ portfolio/src/Screens/Portfolio.jsx
@@ -16,7 +16,7 @@ export default function Portfolio() {
       <div className='proj-container'>
         <div className='proj-title'>Striva</div>
         <a href='https://striva.netlify.app/' target='_blank' rel='noreferrer'><img src='https://i.imgur.com/K51bRMa.png' alt='striva' className='proj-img'></img></a>
-        <div className='desc'>*Striva is best enjoyed in mobile view!* <br></br>
+        <div className='desc'>**Striva is best enjoyed in mobile view!** <br></br>
           Striva is a full-stack Ruby on Rails and React application that is essentially a social media for your activities!
         </div>
         <div className='buttons-container'>
@@ -27,7 +27,7 @@ export default function Portfolio() {
       <div className='proj-container'>
         <div className='proj-title'>AvySavvy</div>
         <a href='https://avy-savvy-snow.netlify.app/' target='_blank' rel='noreferrer'><img src='https://i.imgur.com/86cSM8J.png' alt='avy-savvy' className='proj-img'></img></a>
-        <div className='desc'> AvySavvy is a full-stack application with a React front end and Airtable backend. </div>
+        <div className='desc'> AvySavvy is a full-stack application with a React front end and Airtable backend. Avysavvy provides a platform for users to report avalanches and snowpack observations.</div>
         <div className='buttons-container'>
           <button className='code'><a href='https://github.com/hutchskistoke/avy-savy' target='_blank' rel="noreferrer">see the code</a></button>
           <button className='site'><a href='https://avy-savvy-snow.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
