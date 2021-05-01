35f7a227946755f573adfb305af4b29c678434e3
Ashley Hutchinson <hutchski@Ashleys-MacBook-Pro.local>
2021-05-01 09:56:42 -0600

    desc updates

--- portfolio/src/Screens/Portfolio.jsx
+++ portfolio/src/Screens/Portfolio.jsx
@@ -7,7 +7,7 @@ export default function Portfolio() {
       <div className='proj-container'>
         <div className='proj-title'>Pets-R-Us</div>
         <a href='https://pets-r-us.netlify.app/' target='_blank' rel='noreferrer'><img src='https://i.imgur.com/cgZ15Kk.png' alt='pets-r-us' className='proj-img'></img></a>
-        <div className='desc'>Pets-R-Us is a full-stack application built by myself and 3 other team members. </div>
+        <div className='desc'>Pets-R-Us is a full-stack application built by myself and 3 other team members. It utilizes an Express/MongoDB/Mongoose backend and React frontend.</div>
         <div className='buttons-container'>
           <button className='code'><a href='https://github.com/jessicamyee/pets-r-us' target='_blank' rel="noreferrer">see the code</a></button>
           <button className='site'><a href='https://pets-r-us.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
@@ -16,7 +16,9 @@ export default function Portfolio() {
       <div className='proj-container'>
         <div className='proj-title'>Striva</div>
         <a href='https://striva.netlify.app/' target='_blank' rel='noreferrer'><img src='https://i.imgur.com/K51bRMa.png' alt='striva' className='proj-img'></img></a>
-        <div className='desc'>Striva is a mobile first full-stack web application.  </div>
+        <div className='desc'>*Striva is best enjoyed in mobile view!* <br></br>
+          Striva is a full-stack Ruby on Rails and React application that is essentially a social media for your activities!
+        </div>
         <div className='buttons-container'>
           <button className='code'><a href='https://github.com/hutchskistoke/Striva' target='_blank' rel="noreferrer">see the code</a></button>
           <button className='site'><a href='https://striva.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
@@ -25,7 +27,7 @@ export default function Portfolio() {
       <div className='proj-container'>
         <div className='proj-title'>AvySavvy</div>
         <a href='https://avy-savvy-snow.netlify.app/' target='_blank' rel='noreferrer'><img src='https://i.imgur.com/86cSM8J.png' alt='avy-savvy' className='proj-img'></img></a>
-        <div className='desc'>AvySavvy </div>
+        <div className='desc'> AvySavvy is a full-stack application with a React front end and Airtable backend. </div>
         <div className='buttons-container'>
           <button className='code'><a href='https://github.com/hutchskistoke/avy-savy' target='_blank' rel="noreferrer">see the code</a></button>
           <button className='site'><a href='https://avy-savvy-snow.netlify.app/' target='_blank' rel="noreferrer">see it in action!</a></button>
